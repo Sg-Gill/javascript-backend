d7c7a395483b2efb1b70ee31725d3ff86e3ee2c2
Sarang <saranggill92@gmail.com>
2024-04-03 23:16:31 +0500

    controller completed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,71 @@
 import asyncHandler from "../utils/asynchandler.js";
-
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.video.js";
+import { uploadFileOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
+  // get data
+  // validate data
+  // check if user exists
+  // check images
+  // upload images
+  // create user object - create entry in db
+  // remove password and refresh token from response
+  // check for user creation
+  // send response
+
+  const { fullName, email, userName, password } = req.body;
+
+  if (
+    [fullName, email, userName, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  // can also pass whatever i want in the object
+  const existingUser = User.findOne({
+    $or: [{ userName }, { email }],
   });
+  if (existingUser) {
+    throw new ApiError(409, "User with email or username already exists");
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  const avatar = await uploadFileOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadFileOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage.url,
+    email,
+    password,
+    userName: userName.toLowerCase(),
+  });
+
+  // we chain select to get the fields from db and add -whatever the name to deselect
+  // cause by default all are selected the _id is assigned by mongo to all objects added to it
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while creating the user");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered successfully!"));
 });
 
 export { registerUser };
